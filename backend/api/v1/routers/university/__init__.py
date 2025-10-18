2d7831aaa58b8091454cfe9be261c34103e71153
magomedov marif <magomedovmarif2@gmail.com>
2025-10-19 00:52:09 +0300

    fix

--- backend/api/v1/routers/university/__init__.py
+++ backend/api/v1/routers/university/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from backend.api.v1.routers.university.student import router as student_router
 from backend.api.v1.routers.university.applications import router as applications_router
 
-university_v1_router = APIRouter(prefix="/university", tags=["university"])
+university_v1_router = APIRouter(prefix="/api/v1/university", tags=["university"])
 
-university_v1_router.include_router(student_router, prefix="/students")
-university_v1_router.include_router(applications_router, prefix="/applications")
+university_v1_router.include_router(student_router, tags=['university'], prefix='/students')
+university_v1_router.include_router(applications_router, tags=['university'], prefix='/applications')
